c32fdfe1fb8b69dc47aba71beef47a72d4146fce
Jonathan Bossenger <jonathanbossenger@gmail.com>
2019-05-17 20:29:43 +0200

    Adding very basic form

--- wceu-http-workshop.php
+++ wceu-http-workshop.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Wceu Http Workshop
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://jonathanbossenger.com
+ * Description:     Simple Shortcode based newsletter subscribe form that connets to the MailChimp API to subscribe a user
+ * Author:          Jonathan Bossenger
+ * Author URI:      https://jonathanbossenger.com
  * Text Domain:     wceu-http-workshop
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +12,23 @@
  * @package         Wceu_Http_Workshop
  */
 
-// Your code starts here.
+/**
+ * Step 1: Let's build a simple form
+ */
+add_shortcode( 'wceu_form_shortcode', 'wceu_form_shortcode' );
+function wceu_form_shortcode() {
+	ob_start();
+	?>
+	<form>
+		<div>
+			<label for="email">Email address</label>
+			<input type="text" id="email" name="email" placeholder="Email address">
+		</div>
+		<div>
+			<input type="submit" id="submit" name="submit" value="Submit">
+		</div>
+	</form>
+	<?php
+	$form = ob_get_clean();
+	return $form;
+}
