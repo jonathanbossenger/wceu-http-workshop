a2f079ce8031082719c8b4dde77ab5e41e85aa66
Jonathan Bossenger <jonathanbossenger@gmail.com>
2019-05-17 20:01:17 +0200

    Initial scaffold commit

---
+++ wceu-http-workshop.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wceu Http Workshop
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wceu-http-workshop
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wceu_Http_Workshop
+ */
+
+// Your code starts here.
